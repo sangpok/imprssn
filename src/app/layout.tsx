8d715aba5233ab79563592cf070ac6aa4ee247a3
sangpok <48979587+sangpok@users.noreply.github.com>
2024-06-25 22:48:25 +0900

    🔧 프로젝트 초기 설정 관련

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,27 @@
 import type { Metadata } from 'next';
 import { Roboto } from 'next/font/google';
 import './globals.css';
+import { Toaster } from '@/components/ui/toaster';
+import { createClient } from '@/lib/supabase/server';
 
-const inter = Roboto({ weight: '400', subsets: ['latin'] });
+const roboto = Roboto({ weight: '400', subsets: ['latin'], variable: '--roboto' });
 
 export const metadata: Metadata = {
   title: 'Imprssn: 인상기록부',
   description: '서로의 인상을 남겨봐요',
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="ko">
-      <body className={inter.className}>{children}</body>
+      <body className={roboto.variable}>
+        {children}
+        <Toaster />
+      </body>
     </html>
   );
 }
