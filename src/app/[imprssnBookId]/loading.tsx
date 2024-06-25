94f7dd5f64c27257e29be39be3cfcd50db369068
sangpok <48979587+sangpok@users.noreply.github.com>
2024-06-25 23:29:40 +0900

    🚸 인상기록부 페이지 접속 시 로딩 페이지 출력

---
+++ src/app/[imprssnBookId]/loading.tsx
@@ -0,0 +1,11 @@
+import { Loader2Icon } from 'lucide-react';
+
+export default function Loading() {
+  // You can add any UI inside Loading, including a Skeleton.
+  return (
+    <main className="min-w-full min-h-dvh flex items-center justify-center">
+      <Loader2Icon className="animate-spin" />
+      <p>인상기록부를 불러오는 중입니다...</p>
+    </main>
+  );
+}
