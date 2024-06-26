38c1d9a91de8c0f351304a3be75a24134bfc7436
sangpok <48979587+sangpok@users.noreply.github.com>
2024-06-26 19:52:20 +0900

    💄 Loading Icon 사이즈 변경

--- src/app/[imprssnBookId]/loading.tsx
+++ src/app/[imprssnBookId]/loading.tsx
@@ -4,7 +4,7 @@ export default function Loading() {
   // You can add any UI inside Loading, including a Skeleton.
   return (
     <main className="min-w-full min-h-dvh flex items-center justify-center">
-      <Loader2Icon className="animate-spin" />
+      <Loader2Icon className="mr-2 h-4 w-4 animate-spin" />
       <p>인상기록부를 불러오는 중입니다...</p>
     </main>
   );
